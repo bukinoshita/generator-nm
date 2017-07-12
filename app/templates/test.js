f9577f1a07442a5f1e2d314fb968fb47179912da
Bu Kinoshita <bukinoshita@gmail.com>
2017-07-11 23:58:54 -0400

    new configs

--- app/templates/test.js
+++ app/templates/test.js
@@ -1,11 +1,13 @@
-import test from 'ava';
-import m from '.';
+'use strict'
+
+import test from 'ava'
+import m from '.'
 
 test('title', t => {
 	const err = t.throws(() => {
 		m(123)
-	}, TypeError);
-	t.is(err.message, 'Expected a string, got number');
+	}, TypeError)
+	t.is(err.message, 'Expected a string, got number')
 
-	t.is(m('unicorns'), 'unicorns & rainbows');
-});
+	t.is(m('unicorns'), 'unicorns & rainbows')
+})
